47a9ba97f01f0e93d4f9a619c4b3bd37234861d4
Abubakar zahid <zahidabubakar342@gmail.com>
2023-10-09 14:51:43 +0800

    exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise4.py
@@ -0,0 +1,7 @@
+# Defining three different string into one string 
+string1 = "Hello"
+string2 = "World"
+string3 = "!"
+# Concatenating the three string into one string
+output_string = string1 + string2 + string3 
+print(output_string)
