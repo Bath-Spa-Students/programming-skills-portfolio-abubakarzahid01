9a5bb1d5538ea8dcf73530bd1025ca56d97cd34d
Abubakar zahid <zahidabubakar342@gmail.com>
2023-10-10 01:50:56 +0800

    exercise5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise5.py
@@ -0,0 +1,8 @@
+# Importing the math module
+import math
+# Accepting the radius of the circule from the user 
+radius = float(input("Enter the radius of the circle:"))
+# Computing the area of the circle 
+area = math.pi * radius ** 2
+# Displaying the area of the circle 
+print("The area of the circle with radius", radius, "is:", area)
