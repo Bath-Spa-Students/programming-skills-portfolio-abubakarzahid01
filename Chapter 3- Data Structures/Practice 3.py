34e2ae83519ec2468d0bd4b67ab8eb3b64c8fbae
Abubakar zahid <zahidabubakar342@gmail.com>
2023-11-10 23:27:40 +0800

    practices

---
+++ Chapter 3- Data Structures/Practice 3.py
@@ -0,0 +1,5 @@
+#Assume names references a list. Write a for loop that displays each element of the list
+names=['shakin', 'abdullah', 'faisal', 'soban', 'usman']
+#for loop that displays each element of the list
+for i in names:
+    print (i)
---
+++ Chapter 3- Data Structures/Practice 4.py
@@ -0,0 +1,9 @@
+#Assume the lists numbers1 has 100 elements and numbers2 is an empty list. Write code that
+#copies the values in numbers to numbers2.
+numbers1=[]
+for i in range (1,100):
+    numbers1.append(i)
+numbers2=[]
+print ("before copying in numbers:",numbers2)
+numbers2=numbers1
+print ("after copying in numbers2:",numbers2)
